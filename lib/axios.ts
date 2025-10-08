33e0366d4ec5a6243cbcf44f0da7399f4c22ebb5
kiplah <kiplahvictor27@gmail.com>
2025-10-08 22:12:38 +0300

    Refactor axios imports to use default export and update axios instance configuration

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
+const axiosInstance = axios.create({
+  baseURL: "http://127.0.0.1:5000/api", // your local backend
+  withCredentials: true,
+});
 
-export const axiosInstance = axios.create({
-    baseURL:"https://guided-backend-9hw5.onrender.com/api",
-    withCredentials:true
-})
+export default axiosInstance;
